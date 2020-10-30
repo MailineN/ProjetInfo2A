5d9fc1691660dd22f15744576c84894f45c2ba45
Maisonnave Chloé <maisonnavechlo@yahoo.fr>
2020-10-30 15:21:16 +0100

    cc

---
+++ Code/app/features/users/player.py
@@ -0,0 +1,22 @@
+
+class Player(Guest):
+    
+    def __init__(self, identifiant):
+        
+        Guest.__init__(self, identifiant, 'Player')
+
+    
+    def loadGame(self):
+        pass
+    
+    def changePassword(self):
+        pass
+    
+    def seeScores(self) :
+        pass
+    
+    def continuePreviousGame(self) :
+        pass
+    
+
+   
---
+++ Code/app/features/users/playerDAO.py
@@ -0,0 +1,17 @@
+
+class PlayerDAO(GuestDAO):
+    
+    """ classe Data Access Object de la classe Player """
+    
+    def __init__(self):
+        
+        GuestDAO.__init__(self)
+    
+    def updateAccountCredentials(self):
+        pass
+    
+    def getAccountData(self):
+        pass
+    
+    
+    
