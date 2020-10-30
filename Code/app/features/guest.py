68a8ad2b9335bc69e68dd594b636481fa345355a
Maisonnave Chloé <maisonnavechlo@yahoo.fr>
2020-10-30 14:21:39 +0100

    essai

---
+++ Code/app/features/guest.py
@@ -0,0 +1,36 @@
+class Guest :
+    
+    """ classe invité, pouvant jouer au jeu sans se connecter """
+    
+    def __init__(self, identifiant, userType):
+        self.identifiant = identifiant
+        self.userType = userType
+    
+    @staticmethod 
+    
+    def play() : 
+        
+        pass
+    
+    def chooseGame():
+        
+        pass
+    
+    def quitter() :
+        pass
+    
+    def createAccount():
+        pass
+    
+    def connexion():
+        pass
+    
+    def initEmptyGame() :
+        pass
+    
+    def joinGame():
+        pass
+    
+    def displayFinishedGame():
+        pass
+    
