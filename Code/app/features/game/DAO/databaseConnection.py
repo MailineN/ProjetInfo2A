b7f5dafa1be2e36777dea4243afa72e6f4885802
MailineN <nguyen.mailine@gmail.com>
2020-11-02 10:05:33 +0100

    Added database Credential

--- Code/app/features/game/DAO/databaseConnection.py
+++ Code/app/features/game/DAO/databaseConnection.py
@@ -1,20 +1,50 @@
 
+import psycopg2 
+
+
 class DatabaseConnection:
     
-    def __init__():
-        pass
-    
-    def getConnexion(self):
-        pass
-    
-    def closeConnexion(self):
-        pass
-    
-    def getInstance(self):
-        pass
-    
-    def saveInstance(self):
-        pass
+    __instance = None
+    @staticmethod
+    def getInstance():
+        
+        if DatabaseConnection.__instance is None:
+            DatabaseConnection()
+        return DatabaseConnection.__instance
+
+
+    @staticmethod
+    def getConnexion():
+        
+        return DatabaseConnection.getInstance().getconn()
+
+    @staticmethod
+    def closeConnexions():
+        
+        try :
+            DatabaseConnection.getInstance().closeall
+            closed = True
+        except Exception :
+            print("Problème lors de la fermeture")
+            closed = False
+        return closed
+
+    @staticmethod
+    def putBackConnexion(connection):
+        DatabaseConnection.getInstance().putconn(connection)
+
+    def __init__(self):
+        if DatabaseConnection.__instance != None:
+            raise Exception("Cette classe est un singleton. Utiliser la "
+                            "méthode getInstance()")
+        else:
+            DatabaseConnection.__instance = psycopg2.pool.SimpleConnectionPool(1, 2,
+                                                                           host='kandula.db.elephantsql.com',
+                                                                           port="5432",
+                                                                           user='rkvzuxom',
+                                                                           password='gxzP6WQlIbmTH6skR39OpzMXTSULsEqy',
+                                                                           cursor_factory=RealDictCursor)
+
     
     
 
