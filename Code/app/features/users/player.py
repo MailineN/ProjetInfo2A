36d26ba54556d1d023bd6a7c93137bd64f5c1b26
Maisonnave Chloé <maisonnavechlo@yahoo.fr>
2020-11-04 16:08:59 +0100

    change password

--- Code/app/features/users/player.py
+++ Code/app/features/users/player.py
@@ -10,6 +10,19 @@ class Player(Guest):
         pass
     
     def changePassword(self):
+        """ Changer le mot de passe d'un utilisateur """
+        motdepasse = input("Entrez votre mot de passe actuel")
+        new_mdp = input("Entrez votre nouveau mot de passe")
+
+        m = hashlib.md5()
+        m.update(motdepasse)
+        hash_mdp = m.digest()
+        m.update(new_mdp)
+        new_hash_mdp = m.digest
+
+        #laisser faire la classe playerDAO
+        PlayerDAO.updatePassword(hash_mdp, new_hash_mdp)
+
         pass
     
     def seeScores(self) :
