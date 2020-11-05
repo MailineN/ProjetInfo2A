cd7db72c2e16c295aa0868d0dea9d5cce1aed89e
CamiliaKashi <camilia.kashi@wanadoo.fr>
2020-11-05 12:18:38 +0100

    admins changes

--- Code/app/features/users/admin.py
+++ Code/app/features/users/admin.py
@@ -14,7 +14,7 @@ class Admin(Player):
         Player.__init__(self, identifiant, "Admin") #il hérite de l'attribut userType
 
     def createUserAccount():
-        
+       """ fct à changer par rapport à la view ??? """ 
         #demander si on veut créer un joueur ou un admin
         userType = input("voulez vous créer un Joueur ou un Admin ?") #faire un truc pr que tout passe selon si ils mettent des maj et tout 
 
@@ -40,27 +40,21 @@ class Admin(Player):
             m.update(mdp)
             hash_mdp = m.digest()
 
-        #ajouter le nouveau compte à la base
-        GuestDAO.addAccounttoData(username,hash_mdp)
+        #ajouter le nouveau compte à la base en appelant la fonction de guestDAO
+        GuestDAO.addAccounttoData(username,mdp)
         return("Le compte a bien été créé")
 
-        
-        pass
-    
-
     def deleteUserAccount():
-       """utiliser la fct "displayDeleteUserAccount" de admin view pr parler à l'utilisateur
-          utiliser la fct "deleteUserAccount" de admin dao pr supprimer le compte dans la base de données"""
-        adminView.displayDeleteUserAccount()        
-        adminDAO.deleteUserAccount(username)
-         
-        
-        pass
+        #la view va aller demander à l'utilisateur quel compte il veut supprimer à partir de son username
+        adminView.displayDeleteUserAccount(username,mdp)           
+        #la fonction de adminDAO va aller supprimer ce compte dans la base
+        adminDAO.deleteUserAccount(username,mdp)
+        return("Le compte a bien été supprimé") 
     
     def seeUserAccount():
-        """utiliser la fct "displaySeeUserAccount" de admin view pr parler à l'utilisateur
-        utiliser la fct "getAllUserData" de admin dao pr avoir accès aux informations de l'utlisateur"""
-        adminView.displaySeeUserAccount()
-        adminDAO.getAllUserData(username)
+        #la view va aller demander à l'utilisateur quel compte il veut consulter à partir de son username
+        adminView.displaySeeUserAccount(username,mdp)
+        #la fonction de adminDAO va aller récupérer ces informations dans la base (et les retourner ? ou il faut que je fasse un return ici?)
+        adminDAO.getAllUserData(username,mdp)
 
-        pass
+
