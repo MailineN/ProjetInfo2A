79778f4ce71b8ff34017699450afaf1e06ecb0cc
Corentin NIO <coco.nio@live.fr>
2020-11-05 11:25:15 +0100

    save

--- Code/app/features/DAO/pile_dao.py
+++ Code/app/features/DAO/pile_dao.py
@@ -1,16 +1,18 @@
 import psycopg2
 from databaseConnection import DatabaseConnection
-
-
+ 
+ 
 class PileDAO():
 
+ 
     def savePileinDataBase(pile):
         connexion = DatabaseConnection.getConnexion()
         curseur = connexion.curseur()
         try:
             curseur.execute(
-                "INSERT INTO Piles (idPile,idGame,card1,card2,card3,card4) \
-                VALUES (pile.idPile,pile.idGame,pile.card_list[0],pile.card_list[1],pile.card_list[2],pile.card_list[3]) RETURNING idPile;"
+                "INSERT INTO pile (idPile, idGame, card_list)"
+                "VALUES (%s, %s, %s) RETURNING idPile "
+                (pile.idPile, pile.idGame, pile.card_list)
             )
 
             pile.id = curseur.fetchone()["idPile"]
@@ -22,21 +24,21 @@ class PileDAO():
             curseur.close
             DatabaseConnection.putBackConnexion(connexion)
 
+
+
     def getPreviousPiles(id):
         pass connexion = DatabaseConnection.getConnexion()
         curseur = connexion.curseur()
         try:
             curseur.execute(
-                "SELECT list_card FROM pile WHERE idGame=id;"
+                "SELECT idPile, idGame, card_list FROM pile WHERE idGame=id;"
             )
-
+        
             resultats = curseur.fetchall()
             PreviousPiles = []
-            for resultat in resultats:
-                PreviousPiles.append(resultat["list_card"])
-        except psycopg2.Error as error:
-            connexion.rollback()
-            raise error
+            for resultat in resultats :
+                PreviousPiles.append(resultat)
         finally:
             curseur.close
             DatabaseConnection.putBackConnexion(connexion)
+        return(PreviousPiles)
