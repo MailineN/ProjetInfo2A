6d9fa8b96a75ac5b99d6649b65197517e2bf0cd4
CamiliaKashi <camilia.kashi@wanadoo.fr>
2020-11-05 10:50:00 +0100

    admin

--- Code/app/features/users/admin.py
+++ Code/app/features/users/admin.py
@@ -1,6 +1,7 @@
 from app.security.id import verif_init_id
 from app.security.mdp import verif_init_mdp
 from app.DAO.guestDao import guestDAO
+from app.DAO.adminDAO import adminDAO
 import hashlib
 
 class Admin(Player):
@@ -27,24 +28,24 @@ class Admin(Player):
 
         if userType == "Player":
             #création du compte d'un joueur
-            PlayerUsername = verif_id(input("Entrez le nom d'utilisateur")) 
-            PlayerMotdepasse = input("Choisissez le mot de passe")
-            verifMotdepasse= input("Réécrivez le mot de passe")
-            motdepasse = verif_init_mdp(motdepasse, verifMotdepasse) #voir que le mdp est bon
+            username = verif_id(input("Entrez le nom d'utilisateur")) 
+            mdp = input("Choisissez le mot de passe")
+            verifmdp= input("Réécrivez le mot de passe")
+            mdp = verif_init_mdp(mdp, verifmdp) #voir que le mdp est bon
             #hashage du mdp choisi
             m = hashlib.md5()
-            m.update(PlayerMotdepasse)
+            m.update(mdp)
             hash_mdp = m.digest()
 
         elif userType == "Admin":
             #création du compte d'un admin
-            AdminUsername = verif_id(input("Entrez le nom d'utilisateur")) 
-            AdminMotdepasse = input("Choisissez le mot de passe")
-            verifMotdepasse= input("Réécrivez le mot de passe")
-            motdepasse = verif_init_mdp(motdepasse, verifMotdepasse) #voir que le mdp est bon
+            username = verif_id(input("Entrez le nom d'utilisateur")) 
+            mdp = input("Choisissez le mot de passe")
+            verifmdp= input("Réécrivez le mot de passe")
+            mdp = verif_init_mdp(mdp, verifmdp) #voir que le mdp est bon
             #hashage du mdp choisi
             m = hashlib.md5()
-            m.update(AdminMotdepasse)
+            m.update(mdp)
             hash_mdp = m.digest()
 
         #ajouter le nouveau compte à la base
@@ -58,7 +59,7 @@ class Admin(Player):
     def deletePlayerAccount():
 
 """moitié ds la DAO moitié ici"""
-
+        adminDAO.getAllUserData(username) 
         
         pass
     
