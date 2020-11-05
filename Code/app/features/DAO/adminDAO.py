18dea3253c553554afe243516a971c252eea7201
CamiliaKashi <camilia.kashi@wanadoo.fr>
2020-11-05 11:41:19 +0100

    admins changements

--- Code/app/features/DAO/adminDAO.py
+++ Code/app/features/DAO/adminDAO.py
@@ -46,3 +46,6 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     getAllUserData()
+
+
+    def deleteUserAccount():
--- Code/app/features/users/adminView.py
+++ Code/app/features/users/adminView.py
@@ -5,7 +5,7 @@ from examples import custom_style_2
 
 
 class AdminView():
-        def displaydisplayCreateUserAccount():
+        def displayCreateUserAccount():
             questions = [
                 {
                     'type': 'input',
@@ -38,30 +38,39 @@ class AdminView():
             questions = [
                 {
                     'type': 'input',
-                    'name': 'Usertype',
-                    'message': 'Voulez-vous créer un Player ou un Admin?', #?? jsp si ca se passe comme ca qd c'est un choix
+                    'name': 'Username',
+                    'message': 'Entrez le nom d\'utilisateur du compte que vous voulez supprimer:', #?? jsp si ca se passe comme ca qd c'est un choix
                 },                
-                {
-                    'type': 'input',
-                    'name': 'Pseudo',
-                    'message': 'Entrez le nom d\'utilisateur : ',
-                },
                 {
                     'type': 'password',
                     'name': 'Mdp',
-                    'message': 'Entrez le mot de passe : ',
+                    'message': 'Entrez le mot de passe associé  à ce compte: ',
 
                 },
+
+            ]
+            print("Suppression de compte : \n")
+            res = prompt(questions, style=custom_style_2)
+            return(res['Username'], res['Mdp'])
+
+        def displaySeeUserAccount():
+            questions = [
+                {
+                    'type': 'input',
+                    'name': 'Username',
+                    'message': 'Entrez le nom d\'utilisateur du compte que vous voulez consulter:', #?? jsp si ca se passe comme ca qd c'est un choix
+                },                
                 {
                     'type': 'password',
-                    'name': 'VerifMdp',
-                    'message': 'Vérifiez le mot de passe : ',
+                    'name': 'Mdp',
+                    'message': 'Entrez le mot de passe associé  à ce compte: ',
 
-                }
+                },
+
             ]
-            print("Création de compte : \n")
+            print("Consultation de compte : \n")
             res = prompt(questions, style=custom_style_2)
-            return(res['Usertype'], res['Pseudo'], res['Mdp'], res['VerifMdp'])
+            return(res['Username'], res['Mdp'])
 
 
 
