fdd7ad904b1f95c7b61250a955874142195db9f8
Mailine <nguyen.mailine@gmail.com>
2020-11-06 11:45:09 +0100

    Test vérifié du chargement

--- Code/app/features/game/gameMechanics/chargement.py
+++ Code/app/features/game/gameMechanics/chargement.py
@@ -1,10 +1,10 @@
 from app.features.DAO.gameDAO import gameDAO
 from app.menu.menu_interface import MenuInterface
 from app.menu.menu_data import menu
-from threading import _Timer
+from threading import Timer
 
 
-class CustomThread(_Timer):
+class CustomThread(Timer):
     def __init__(self, interval, function, args=[], kwargs={}):
         self._original_function = function
         super(CustomThread, self).__init__(
@@ -22,8 +22,7 @@ def chargement(idGame, nameGame):
     print("*******************************************************\n")
     print("La partie commencera lorsqu'il y aura assez de joueurs\n")
     print("*******************************************************\n \n ")
-    c = CustomThread(60.0, gameDAO.fetchNumberPlayer(idGame)).start()
-    result = c.results(),
+    result = gameDAO.fetchNumberPlayer(idGame),
     if not result[0]:
         # Pour l'instant reviens au menu principal à tester après
         print('La partie que vous souhaitez rejoindre a déjà commencé\n')
@@ -32,6 +31,9 @@ def chargement(idGame, nameGame):
     else:
         if nameGame == "Belote":
             while result[1] < 4:
+				c = CustomThread(60.0, gameDAO.fetchNumberPlayer(idGame)).start()
+				c.start()
+				result = c.join()
                 print(
                     "*******************************************************\n")
                 print(
@@ -39,7 +41,7 @@ def chargement(idGame, nameGame):
                 print('Il y a actuellement '+result[1]+'joueurs \n')
                 print(
                     "*******************************************************\n \n")
-            c.cancel()
+            try : c.cancel()
             sortie = True
     return sortie
 
--- Code/testMain.py
+++ Code/testMain.py
@@ -1,7 +1,28 @@
 """Ce fichier sert à tester des fonctions individuelles du code 
     """
-from app.features.game.apiInteractions.handView import HandView
-from app.features.game.gameMechanics.chargement import chargement
+from threading import Timer
+
+
+class CustomTimer(Timer):
+    def __init__(self, interval, function, args=[], kwargs={}):
+        self._original_function = function
+        super(CustomTimer, self).__init__(
+            interval, self._do_execute, args, kwargs)
+
+    def _do_execute(self, *a, **kw):
+        self.result = self._original_function(*a, **kw)
+
+    def join(self):
+        super(CustomTimer, self).join()
+        return self.result
+
+
+def add_together(a, b):
+    return a + b
+
 
 if __name__ == "__main__":
-    chargement(1,'Belote')
+    while True : # Boucle infinie
+        c = CustomTimer(10, add_together, (2, 4))
+        c.start()
+        print(c.join())
