dac80fd3b5c13a0f5cd445d7003b19559be4f15e
Souliman <souliman.eljarroudi@gmail.com>
2020-11-06 12:08:54 +0100

    test

--- Code/tests/test_Belote.py
+++ Code/tests/test_Belote.py
@@ -38,46 +38,10 @@ class BeloteTests(unittest.TestCase):
 
         self.assertEqual((15, 3), Belote.countPoint(Belote(), plis, atout))
 
-    
+
     def testmonpote(self):
         team1 = ["player1","player2"]
         team2=[]
         testmaitre = "player2"
-        maitre = Belote.monpote("player1", testmaitre, team1, team2)
-        self.assertEqual(testmaitre,maitre)
+        self.assertEqual(testmaitre,Belote.monpote("player1", testmaitre, team1, team2))
 
-    def testa_de_latout(self):
-        atout = "HEARTS"
-        testplayer = "testplayer"
-        testplayer.handList = [
-        Card(valeur="ACE", couleur="DIAMONDS"),
-        Card(valeur="KING", couleur="DIAMONDS"),
-        Card(valeur="8", couleur="DIAMONDS"),
-        Card(valeur="7", couleur="HEARTS")
-        ]
-        self.assertTrue(Belote.a_de_latout(testplayer,atout))
-
-    def testa_lacouleur(self):
-        couleur = "HEARTS"
-        testplayer = "testplayer"
-        testplayer.handList = [
-        Card(valeur="ACE", couleur="DIAMONDS"),
-        Card(valeur="KING", couleur="DIAMONDS"),
-        Card(valeur="8", couleur="DIAMONDS"),
-        Card(valeur="7", couleur="HEARTS")
-        ]
-        self.assertTrue(Belote.a_lacouleur(testplayer,couleur))
-
-    def testmonteratout(self):
-        testplayer = "testplayer"
-        vcarte = 11
-        atout = "HEARTS"
-        testplayer.handList = [
-        Card(valeur="ACE", couleur="DIAMONDS"),
-        Card(valeur="KING", couleur="DIAMONDS"),
-        Card(valeur="8", couleur="DIAMONDS"),
-        Card(valeur="JACK", couleur="HEARTS")
-        ]
-        self.point_atout = {"JACK": 20, "9": 14, "ACE": 11,
-                        "10": 10, "KING": 4, "QUEEN": 3, "8": 0, "7": 0}
-        self.assertTrue(Belote.monteratout(testplayer,vcarte))
