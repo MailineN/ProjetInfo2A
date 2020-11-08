5bc096bef9fd2f4ab1945f6c5caeba5695b72c38
Mailine <nguyen.mailine@gmail.com>
2020-11-08 12:40:35 +0100

    Ajout Test database connection

---
+++ Code/tests/test_DataBaseConnection.py
@@ -0,0 +1,21 @@
+import unittest
+from app.features.DAO.databaseConnection import DatabaseConnection
+
+
+class CardsObjectsTests(unittest.TestCase):
+
+    def test_getInstance(self):
+        # GIVEN
+        reservoir_connexion = DatabaseConnection.getInstance()
+
+        # THEN
+        self.assertIsNotNone(reservoir_connexion)
+
+
+    def test_getConnexion(self):
+        # GIVEN
+        connexion = DatabaseConnection.getConnexion()
+
+        # THEN
+        self.assertIsNotNone(connexion)
+
