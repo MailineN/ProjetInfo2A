42ed5a2b1fb016fc7b601fad687550c1f104c76e
CamiliaKashi <camilia.kashi@wanadoo.fr>
2020-11-08 13:53:44 +0100

    admintest

--- Code/tests/test_admin.py
+++ Code/tests/test_admin.py
@@ -5,8 +5,13 @@ from app.features.DAO.adminDAO import adminDAO
 
 class AdminTest(unittest.TestCase):
 
+    #pr tester la méthode createUserAccount de la classe Admin, on test la méthode createUserAccount de GuestDAO (?)
     def testCreateUserAccount(self):
-        self.assertEqual("le compte a bien été créé", CreateUserAccount()) #mettre des valeurs
-    
-    def
-        testDeleteUserAccount
+        #à faire dans test_guest
+
+    #pr tester la méthode deleteUserAccount de la classe Admin, on test la méthode deleteUserAccount de AdminDAO (?)
+    def testDeleteUserAccount(self):
+        self.assertEqual("Le compte a bien été supprimé", AdminDAO.DeleteUserAccount())#mettre des valeurs
+
+    def testSeeUserAccount(self):
+        self.assertEqual()
