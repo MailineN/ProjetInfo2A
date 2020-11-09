15cf24b55ed9cd05e3a5111c60ac622a598eea91
Mailine <nguyen.mailine@gmail.com>
2020-11-09 18:57:13 +0100

    Rename tests

--- Code/tests/test_pileDAO.py
+++ Code/tests/test_pileDAO.py
@@ -4,22 +4,16 @@ import psycopg2
 from app.features.DAO.databaseConnection import DatabaseConnection
 
 
-
-
-
-class testpileDAO(unittest.TestCase):
+class PileDAOTests(unittest.TestCase):
     """Test les fonction de la classe PileDAO"""
-    
 
     def testnewPile(self):
-        
+
         test = "1"
         test_pile = PileDAO.newPile(test)
         self.assertIsNotNone(test_pile.idPile)
         deleted = PileDAO.delete(test_pile)
         self.assertTrue(deleted)
-
-    
 
     def testdelete(self):
         test = "00000"
