7eafd8f6a1c3add3d48b1650c170c63f5023e807
Maisonnave Chloé <maisonnavechlo@yahoo.fr>
2020-11-10 16:37:07 +0100

    test

--- Code/tests/test_guestDAO.py
+++ Code/tests/test_guestDAO.py
@@ -4,9 +4,12 @@ from app.features.DAO.guestDAO import GuestDAO
 
 class TestGuest(unittest.TestCase):
     """Teste les fonctions de la classe GuestDAO"""
-    def testAddAccounttoData(self):
+    def testAddAccounttoData():
         self.assertEqual(GuestDAO.addAccounttoData("chloé","3107"), "Votre compte a bien été créé")
 
 
     def testCheckAccounttoData():
         self.assertEqual(GuestDAO.checkAccounttoData("chloé","3107"), "Vous êtes connecté")
+
+    def testAddGame():
+        self.assertEqual(GuestDAO.addGame(),)
