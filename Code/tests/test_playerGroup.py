918288c0f452f0be76d0856a2ae021aa62b4d19c
Corentin NIO <coco.nio@live.fr>
2020-11-11 16:56:05 +0100

    save

--- Code/tests/test_playerGroup.py
+++ Code/tests/test_playerGroup.py
@@ -6,7 +6,7 @@ from app.features.users.player import Player
 class TestGuest(unittest.TestCase):
 
     def testAddPlayer(self) :
-        player1 = Player(1,[])
+        player1 = Player(identifiant = 1,handList = [])
         player2 = Player(2,[])
         player3 = Player(3,[])
         playerGroup1 = PlayerGroup([player1,player2])
