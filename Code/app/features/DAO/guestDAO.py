c04809f1fdecd6b30d7017558187703319f40c35
Souliman <souliman.eljarroudi@gmail.com>
2020-11-14 17:40:58 +0100

    correction

--- Code/app/features/DAO/guestDAO.py
+++ Code/app/features/DAO/guestDAO.py
@@ -37,17 +37,15 @@ class GuestDAO:
         curseur = connexion.cursor()
         try:
             curseur.execute(
-                "Select mdp FROM users WHERE username = %s ", ((username),))
-            resultat = curseur.fetchone()
-            if resultat:
-                ans = resultat[0]
-                if ans == mdp:
-                    curseur.execute(
-                        "Select id_users from users WHERE username = %s ", ((username),))
-                    id_user = curseur.fetchone()[0]
-                    curseur.execute("UPDATE users SET connected = TRUE WHERE username = %s ", ((username),))
-                    connexion.commit()
-                    return(id_user)
+                "Select mdp from users WHERE username = %s ", (username,))
+            ans = curseur.fetchone()[0]
+            if ans == mdp:
+                curseur.execute(
+                    "Select id_users from users WHERE username = %s ", (username,))
+                id_user = curseur.fetchone()[0]
+                curseur.execute("UPDATE users SET connected = TRUE WHERE username = %s ", (username,))
+                connexion.commit()
+                return id_user
         finally:
             curseur.close
             DatabaseConnection.putBackConnexion(connexion)
