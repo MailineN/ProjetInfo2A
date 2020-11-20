bf7ecf9fcae3d2e0a1ffc4332e4d7e9077599e59
Corentin NIO <coco.nio@live.fr>
2020-11-20 15:34:10 +0100

    save

--- Code/tests/test_hand.py
+++ Code/tests/test_hand.py
@@ -4,7 +4,7 @@ from app.features.game.cardObjects.cards import Card
 
 class HandsObjectsTests(unittest.TestCase):
 
-    def testCard(self):
+    def testHand(self):
         hand = Hand(1,1,1,[Card(valeur="ACE", couleur="CLUBS"),Card(valeur="JACK", couleur="SPADES")])
         self.assertEqual(str(hand), "Les cartes de la main sont : ACE de CLUBS, JACK de SPADES, ")
 
