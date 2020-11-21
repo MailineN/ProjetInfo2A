d6fcca2e409f8c00e0033252c6b7a15f541029b4
Mailine <nguyen.mailine@gmail.com>
2020-11-21 09:44:39 +0100

    Ajout de quoi sauvegarder et charger les piles

--- Code/app/features/DAO/handDAO.py
+++ Code/app/features/DAO/handDAO.py
@@ -1,25 +1,41 @@
 import psycopg2
 from app.features.DAO.databaseConnection import DatabaseConnection
 
-
 class HandDAO:
-       
-    """ 
-        deux méthodes:
-        saveHandinDatabase()
-        getPreviousHandfromDatabase()
-    
-    """
-    
-    def saveHandinDatabase(hand):
+
+    @staticmethod
+    def newHand(idjeu):
         connexion = DatabaseConnection.getConnexion()
         curseur = connexion.cursor()
         try:
             curseur.execute(
-                "INSERT INTO hand (idHand, idGame, idPlayer, card_list)"
-                "VALUES (%s, %s, %s, %s) RETURNING idHand ",
-                (hand.idHand, hand.idGame, hand.idPlayer, hand.card_list,)
+                "INSERT INTO Hands (idGame)"
+                "VALUES (%s) RETURNING idHand ",
+                (idjeu,)
+                # On récupère l'id de la hand
             )
+            idHand = curseur.fetchone()[0]
+            connexion.commit()
+        except psycopg2.Error as error:
+            # la transaction est annulée
+            connexion.rollback()
+            raise error
+        finally:
+            curseur.close()
+            DatabaseConnection.putBackConnexion(connexion)
+        return idHand
+
+    @staticmethod
+    def savehandinDataBase(hand, listCard):
+        connexion = DatabaseConnection.getConnexion()
+        curseur = connexion.cursor()
+        try:
+            curseur.execute(
+                "INSERT INTO hands (idhand, idGame, listCard)"
+                "VALUES (%s, %s, %s) RETURNING idhand ",
+                (hand.idhand, hand.idGame, listCard)
+            )
+
             hand.id = curseur.fetchone()[0]
             connexion.commit()
         except psycopg2.Error as error:
@@ -28,3 +44,42 @@ class HandDAO:
         finally:
             curseur.close
             DatabaseConnection.putBackConnexion(connexion)
+
+    @staticmethod
+    def getHand(idPlayer, idGame):
+        connexion = DatabaseConnection.getConnexion()
+        curseur = connexion.cursor()
+        try:
+            curseur.execute(
+                "SELECT * FROM hands WHERE idPlayer=%s AND idGame = %s RETURNING listCard", (
+                    idPlayer, idGame)
+            )
+
+            resultats = curseur.fetchall()
+        finally:
+            curseur.close
+            DatabaseConnection.putBackConnexion(connexion)
+        return(resultats)
+
+    @staticmethod
+    def delete(idHand):
+        deleted = False
+        connexion = DatabaseConnection.getConnexion()
+        curseur = connexion.cursor()
+        try:
+            curseur.execute(
+                "DELETE FROM hands WHERE idHand=%s", (idHand,)
+            )
+
+            if curseur.rowcount > 0:
+                deleted = True
+
+            connexion.commit()
+        except psycopg2.Error as error:
+            connexion.rollback()
+            raise error
+        finally:
+            curseur.close()
+            DatabaseConnection.putBackConnexion(connexion)
+
+        return deleted
--- Code/app/features/game/cardObjects/handPile.py
+++ Code/app/features/game/cardObjects/handPile.py
@@ -1,34 +1,45 @@
 from app.features.game.cardObjects.deck import PileCard
 from app.features.game.cardObjects.cards import Card
 from app.features.DAO.pileDAO import PileDAO
+from app.features.DAO.handDAO import HandDAO
 
 
 class Hand(PileCard):
 
-    def __init__(self, idHand, idGame, idPlayer, card_list):
-        super().__init__(cards=card_list, idend=idHand)
+    def __init__(self, idGame, idHand, card_list=[]):
         self.idGame = idGame
-        self.idPlayer = idPlayer
-
-    def addCard(self, card):
-        self.cards.append(card)
+        self.idPile = idHand
+        self.card_list = card_list
 
     def __str__(self):
-        rep = "Les cartes de la main sont : "
+        """ Permet de montrer les cartes de la main 
+        """
+        rep = "Les cartes de la main sont : \n"
         liste = ""
-        for card in self.cards:
+        for card in self.card_list:
             liste += str(card) + ", "
         return(rep+liste)
 
-    def poser(self, card, pile):
-        ide = card.code
-        newhand = []
-        for i in range(Hand.len()):
-            card = self.card_list[i]
-            if not(ide == card.code):
-                newhand.append(card)
-        self.card_list = newhand
-        pile.addCard(Card)
+    def newHand(idGame):
+        """ Initialise une pile vide
+
+        Args:
+            idGame (str): Identifiant du jeu pour laquelle la pile est crée
+
+        """
+        return HandDAO.newHand(idGame)
+
+    def saveHand(hand):
+        HandDAO.saveHandinDataBase(hand)
+
+    @staticmethod
+    def getHand(idGame, idPlayer):
+        resultats = HandDAO.getHand(idPlayer, idGame)
+        list_card = []
+        for card in resultats.split():
+            list_card.append(Card.toCards(card))
+
+        return list_card
 
 
 class Pile(PileCard):
@@ -72,6 +83,5 @@ class Pile(PileCard):
         """
         return PileDAO.newPile(idGame)
 
-
     def savePile(pile):
-        PileDAO.savePileinDataBase(pile)    
+        PileDAO.savePileinDataBase(pile)
