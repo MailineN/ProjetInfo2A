0a9d8d6a264e47c7a8675ee9bdc96ca01c17e8b2
Mailine <nguyen.mailine@gmail.com>
2020-11-23 14:14:39 +0100

    Correction2

--- Code/tests/test_Belote.py
+++ Code/tests/test_Belote.py
@@ -8,7 +8,7 @@ from app.features.game.cardObjects.handPile import Pile
 class BeloteTests(unittest.TestCase):
 
     def test_PointsAtout(self):
-        players=['a','b','a','b']
+        players = ['a', 'b', 'a', 'b']
         atout = "DIAMONDS"
         plis = Pile(None, None, card_list=[
             Card(valeur="ACE", couleur="DIAMONDS"),
@@ -16,10 +16,10 @@ class BeloteTests(unittest.TestCase):
             Card(valeur="KING", couleur="DIAMONDS"),
             Card(valeur="7", couleur="DIAMONDS")
         ])
-        self.assertEqual((29, 1), Belote.countPoint(Belote(players= players), plis, atout))
+        self.assertEqual((29, 1), Belote(players=players).countPoint(plis, atout))
 
     def test_PointsNonAtout(self):
-        players=['a','b','a','b']
+        players = ['a', 'b', 'a', 'b']
         atout = "CLUBS"
         plis = Pile(None, None, card_list=[
             Card(valeur="ACE", couleur="DIAMONDS"),
@@ -28,10 +28,10 @@ class BeloteTests(unittest.TestCase):
             Card(valeur="7", couleur="DIAMONDS")
         ])
 
-        self.assertEqual((15, 0), Belote.countPoint(Belote(players= players), plis, atout))
+        self.assertEqual((15, 0), Belote(players=players).countPoint(plis, atout))
 
     def test_PointsCoupe(self):
-        players=['a','b','a','b']
+        players = ['a', 'b', 'a', 'b']
         atout = "HEARTS"
         plis = Pile(None, None, card_list=[
             Card(valeur="ACE", couleur="DIAMONDS"),
@@ -39,7 +39,7 @@ class BeloteTests(unittest.TestCase):
             Card(valeur="8", couleur="DIAMONDS"),
             Card(valeur="7", couleur="HEARTS")
         ])
-        self.assertEqual((15, 3), Belote.countPoint(Belote(players= players), plis, atout))
+        self.assertEqual((15, 3), Belote(players=players).countPoint(plis, atout))
 
     def test_monpote(self):
         team1 = ["player1", "player2"]
@@ -70,7 +70,7 @@ class BeloteTests(unittest.TestCase):
         self.assertTrue(Belote.a_lacouleur(testplayer, couleur))
 
     def test_monteratout(self):
-        players=['a','b','a','b']
+        players = ['a', 'b', 'a', 'b']
         vcarte = 11
         atout = "HEARTS"
         testplayer = Guest(handList=[
@@ -80,8 +80,7 @@ class BeloteTests(unittest.TestCase):
             Card(valeur="7", couleur="HEARTS")
         ])
 
-        self.assertFalse(Belote.monteratout(
-            Belote(players= players), testplayer, vcarte, atout))
+        self.assertFalse(Belote(players=players).monteratout(testplayer, vcarte, atout))
 
     def test_checkPlayerNumber(self):
         testplayers = [1, 2, 3, 4]
